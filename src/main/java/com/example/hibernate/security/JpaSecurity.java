63b1081fe65a95625eb53099462cfd4d53dfd848
Hronoz87 <hromov_andrey@mail.ru>
2021-04-22 16:49:51 +0300

    Security

---
+++ src/main/java/com/example/hibernate/security/JpaSecurity.java
@@ -0,0 +1,24 @@
+package com.example.hibernate.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class JpaSecurity extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+       auth.inMemoryAuthentication().withUser("Andrei").password("{noop}123456").authorities("READ").and()
+               .withUser("Ivan").password("{noop}123456").authorities("WRITE");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.formLogin().and()
+                .authorizeRequests().antMatchers("/persons/by-city").permitAll()
+                .and()
+                .authorizeRequests().anyRequest().authenticated();
+    }
+}
